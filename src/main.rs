2ef93595c169d27272bf98a5471731897aac9ed4
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-11-20 22:22:00 -0800

    Add PyO3 with minimal example

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,21 @@
-fn main() {}
+use pyo3::prelude::*;
+use pyo3::types::IntoPyDict;
+
+fn main() -> Result<(), ()> {
+    Python::with_gil(|py| {
+        main_(py).map_err(|e| {
+            e.print_and_set_sys_last_vars(py);
+        })
+    })
+}
+
+fn main_(py: Python) -> PyResult<()> {
+    let sys = py.import("sys")?;
+    let version: String = sys.get("version")?.extract()?;
+    let locals = [("os", py.import("os")?)].into_py_dict(py);
+    let user: String = py
+        .eval("os.getenv('USER')", None, Some(&locals))?
+        .extract()?;
+    println!("Hello {}, I'm Python {}", user, version);
+    Ok(())
+}
